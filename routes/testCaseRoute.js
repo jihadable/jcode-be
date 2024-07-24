5eb2fa971508b3f4dc88c03377898ad0f10d7ae1
jihadable <umarjihad141004@gmail.com>
2024-07-24 13:39:42 +0700

    add middleware in test case router

--- routes/testCaseRoute.js
+++ routes/testCaseRoute.js
@@ -1,8 +1,9 @@
 const { Router } = require("express")
 const { getTestCasesByProblem } = require("../controllers/testCaseController")
+const verifyTokenMiddlware = require("../middlewares/verifyTokenMiddleware")
 
 const testCaseRouter = Router()
 
-testCaseRouter.get("/:problem_id", getTestCasesByProblem)
+testCaseRouter.get("/:problem_id", verifyTokenMiddlware, getTestCasesByProblem)
 
 module.exports = testCaseRouter
