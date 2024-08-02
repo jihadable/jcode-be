770308a3a74df31c2006409beee292b83036cd30
jihadable <umarjihad141004@gmail.com>
2024-08-02 09:43:49 +0700

    refactor register method

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 const { Router } = require("express")
 const { getUserProfile, register, login, updateUserProfile } = require("../controllers/userController")
 const verifyTokenMiddlware = require("../middlewares/verifyTokenMiddleware")
+const encryptPasswordMiddleware = require("../middlewares/encryptPasswordMiddleware")
 
 const userRouter = Router()
 
 userRouter.get("/", verifyTokenMiddlware, getUserProfile)
 
-userRouter.post("/register", register)
+userRouter.post("/register", encryptPasswordMiddleware, register)
 
 userRouter.post("/login", login)
 
